c3410058c01a32b839502ecfd6a0459ab03abbfd
Danny Rivera <samboard@hotmail.com>
2021-07-15 18:32:20 -0400

    New server

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,7 @@ lock "~> 3.16.0"
 set :application, "myapp"
 set :repo_url, "git@github.com:drivera53/webapp-cripto-rails-backend.git"
 set :branch, "main"
+
 # Deploy to the user's home directory
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
@@ -11,10 +12,6 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
-
-# Optionally, you can symlink your database.yml and/or secrets.yml file from the shared directory during deploy
-# This is useful if you don't want to use ENV variables
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
